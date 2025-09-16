123c467b23cf918c0dae281a97053a8c3587679b
Miguel Menendez <120128265+Miguiiii@users.noreply.github.com>
2025-09-16 11:31:27 -0400

    Add files via upload

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -8,12 +8,36 @@ class Adeline:
         self.act_hiper = lambda x: (m.exp(2*x)-1)/(m.exp(2*x)+1)
         self.act_sig = lambda x: 1/(1+m.exp(-x))
         self.pesos=[]
+        self.pred = []
+        self.err = []
+        self.mse=0
+        self._ask_model_data()
+        self.preddict()
+        self.get_errors()
+        self.mostrar_res()
+        
+        while True:
+            ans=input("Desea entrenar el modelo? (y/n)")
+            if ans.lower()=="n":
+                break
+            elif ans.lower()=="y":
+                self.train()
+                print("Despues de ser entrenado, el modelo resultante fue:")
+                self.mostrar_res()
 
-    def _ask_training_data(self):
-        self.model=f.File.get_file()
+    def mostrar_res(self):
+        print(f"Pesos: {self.pesos}")
+        print(f"Bias {self.bias}")
+        print(f"Predicciones: {self.pred}")
+        print(f"Errores: {self.err}")
+        print(f"Media de los errores cuadrado (MSE): {self.mse}")
+
+    def _ask_model_data(self):
+        self.model=f.File().get_file()
+        self.expect=self.model.pop()
         self.nw:int=len(self.model)-1 #el numero de pesos
         print(f"El Adeline tendrá {self.nw} inputs y 1 neurona")
-        for i in self.nw:
+        for i in range(self.nw):
             while True:
                 try:
                     self.pesos.append(float(input(f"Ingrese un valor para el peso w{i+1}: ")))
@@ -32,6 +56,23 @@ class Adeline:
             except:
                 print("Valor no aceptado, intente de nuevo")
 
+        while True:
+            a=input("Eliga la funcion de activación a utilizar:\n1 .- Escalonada (Ouptut es 1 o 0)\n2 .- Sigmoide (Output de 0 a 1)\n3 .- Sigmoide bipolar (Output de -1 a 1)\nIngrese la selección: ")
+            try:
+                a=int(a)
+                if a not in (1, 2 , 3):
+                    raise Exception
+                break
+            except:
+                print("Valor no aceptado, intente de nuevo")
+        if a==1:
+            self.activate=self.act_esc
+        if a==2:
+            self.activate=self.act_sig
+        if a==3:
+            self.activate=self.act_bisig
+
+    def pre_training(self):
         while True:
             t=input("Ingrese un valor entre 0 y 1 para la tasa de aprendizaje (1 por defecto, presione Enter): ")
             if t=="":
@@ -53,23 +94,6 @@ class Adeline:
                 break
             except:
                 print("Valor no aceptado, intente de nuevo")
-
-        while True:
-            a=input("Eliga la funcion de activación a utilizar:\n1 .- Escalonada (Ouptut es 1 o 0)\n2 .- Sigmoide (Output de 0 a 1)\n 3 .- Sigmoide bipolar (Output de -1 a 1)")
-            try:
-                a=int(a)
-                if a not in (1, 2 , 3):
-                    raise Exception
-                break
-            except:
-                print("Valor no aceptado, intente de nuevo")
-        if a==1:
-            self.activate=self.act_esc
-        if a==2:
-            self.activate=self.act_sig
-        if a==3:
-            self.activate=self.act_bisig
-
         while True:
             e=input("Ingrese un valor para el error mínimo objetivo (0.5 por defecto, presione Enter): ")
             if e=="":
@@ -81,18 +105,23 @@ class Adeline:
             except:
                 print("Valor no aceptado, intente de nuevo")
 
-    def train(self):
-        self._ask_training_data()
-        for whatever in range(self.epoch):
-            self.pred = []
-            for i in len(self.model[0]): #realiza las predicciones con los pesos y el bias actuales
-                yt=sum([self.model[j][i]*self.pesos[j] for j in range(self.nw)])+self.bias
-                yt=self.activate(yt)
-                self.pred.append(yt)
-            self.err = [self.model[-1][i]-self.pred[i] for i in range(len(self.pred))]
+    def preddict(self):
+        for i in range(len(self.model[0])): #realiza las predicciones con los pesos y el bias actuales
+            yt=sum([self.model[j][i]*self.pesos[j] for j in range(self.nw)])+self.bias
+            yt=self.activate(yt)
+            self.pred.append(yt)
 
+    def get_errors(self):
+        self.err = [self.expect[i]-self.pred[i] for i in range(len(self.pred))]
+        self.mse=sum(list(map(lambda x: x*x, self.err)))/len(self.err)
+    
+    def train(self):
+        self.pre_training()
+        for whatever in range(self.epoch):
+            self.pred=[]
+            self.preddict()
             #revisar si el error es suficientemente bajo antes de realizar correciones
-            self.mse=sum(list(map(lambda x: x*x, self.err)))/len(self.err)
+            self.get_errors()
             if self.mse < self.obj_err:
                 break
 
@@ -102,3 +131,6 @@ class Adeline:
         
     def show_graphs(self):
         pass
+
+if __name__=="__main__":
+    a = Adeline()
