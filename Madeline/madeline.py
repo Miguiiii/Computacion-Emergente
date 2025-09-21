ed1ec78383104ec940c2815bc43a80aea3fc1a0e
Miguiiii <miguel.alejandro.menendez@gmail.com>
2025-09-21 16:38:55 -0400

    Clase Neural_Network __init__ y feef_forw implementados

--- Madeline/madeline.py
+++ Madeline/madeline.py
@@ -1,1 +1,33 @@
- 
+import numpy as np
+
+class Neural_Network:
+
+	def __init__(self, n_in:int=1, n_out:int=1, depth:int=1, width:int=1, activation:function=lambda x: 1 if x>0 else 0):
+		self.input_size=n_in
+		self.output_size=n_out
+		self.hidden_size=width
+		self.hidden_depth=depth
+		self.activation = activation
+		
+		self.weights_input_hidden = np.random.randn(n_in, width)
+		self.weights_hidden_hidden = np.array([np.random.rand(width, width) for i in range(depth-1)])
+		self.weights_hidden_output = np.random.randn(width, n_out)
+
+		self.bias_hidden = np.zeros(depth, 1, width)
+		self.bias_output = np.zeros(1, n_out)
+
+	def feed_forw(self, X):
+		res = np.dot(X, self.wights_input_hidden)+self.bias_hidden[0]
+		res = self.activation(res)
+		
+		for n_layer in range(self.hidden_depth-1):
+			res = np.dot(res, self.weights_hidden_hidden[n_layer])+self.bias_hidden[n_layer+1]
+
+		res = np.dot(res, self.weights_hidden_output)+self.bias_output
+		res = self.activaction(res)
+
+	def backprop(self):
+		pass
+
+	def train(self):
+		pass
--- Madeline/np_tests.py
+++ Madeline/np_tests.py
@@ -1,1 +1,49 @@
 import numpy as np 
+
+sig = lambda x: 1/(1+np.exp(-x))
+
+"""X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+WI = np.random.randn(2, 4)
+WO = np.random.randn(4, 1)
+BI = np.zeros((1,1,4))
+BO = np.random.randn(1, 1)
+
+print(X)
+print(WI)
+print(WO)
+print(BI)
+print(BI[0])
+print(BO)
+
+calc = np.dot(X, WI)
+print(calc)"""
+
+
+test = lambda x: 1 if x>0 else 0
+
+print(type(test))
+
+"""print(X.shape)
+print(type(X))
+for i in X:
+	print(i,type(i))
+
+print(X[0])
+
+
+
+t = np.array([[0,0]])
+print(t.shape)
+print(X)
+
+print(WI)
+
+print(WO)
+
+R = np.dot(X, WI)+1
+
+print(R)
+
+R = sig(R)
+
+print(R)"""
